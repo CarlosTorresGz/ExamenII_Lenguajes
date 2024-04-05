2dfe351bbee36d5c0a660368d4ea13119addbbc1
Carlos Torres <carltorres935@gmail.com>
2024-04-05 15:35:45 -0600

    crea proyecto

---
+++ demo5/src/test/java/hn/unah/lenguajes1900/examen2/demo5/Demo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes1900.examen2.demo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
