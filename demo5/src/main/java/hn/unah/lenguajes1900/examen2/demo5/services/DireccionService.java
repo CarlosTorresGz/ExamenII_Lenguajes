b781527eb706f5ffc535469410d6a6a7154c704e
Carlos Torres <carltorres935@gmail.com>
2024-04-05 19:28:17 -0600

    crea DireccionService

---
+++ demo5/src/main/java/hn/unah/lenguajes1900/examen2/demo5/services/DireccionService.java
@@ -0,0 +1,9 @@
+package hn.unah.lenguajes1900.examen2.demo5.services;
+
+import hn.unah.lenguajes1900.examen2.demo5.entities.Direcciones;
+
+public interface DireccionService {
+    
+    public Direcciones creaDirecciones(String dni, Direcciones direccion);
+
+}
